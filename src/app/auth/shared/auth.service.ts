f9702595c420ba4c6ee0d57bcfb0299b374c7ff2
Anwarkamal1 <Anwar2lucky@gmail.com>
2019-04-01 00:19:24 +0500

    modify package-jsonfile and authservice

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import * as jwt from 'jsonwebtoken';
+// import * as jwt from 'jsonwebtoken';
 class DecodedToken {
   exp: number = 0;
   username: string = '';
@@ -15,7 +15,8 @@ export class AuthService {
     //   JSON.parse(localStorage.getItem('swu_meta')) || new DecodedToken();
   }
   private saveToken(token: string): string {
-    this.decodedToken = jwt.decode(token);
+    // this.decodedToken = jwt.decode(token);
+    this.decodedToken = this.parseJwt(token);
     localStorage.setItem('swu_auth', token);
     localStorage.setItem('swu_meta', JSON.stringify(this.decodedToken));
     // console.log(this.parseJwt());
@@ -58,20 +59,20 @@ export class AuthService {
     }
     return null;
   }
-  // parseJwt() {
-  //   const token = this.getJwt();
-  //   if (!token) {
-  //     return null;
-  //   }
-  //   const base64Url = token.split('.')[1];
-  //   const base64 = base64Url.replace('-', '+').replace('_', '/');
-  //   return JSON.parse(window.atob(base64));
-  // }
-  // getJwt() {
-  //   const token = localStorage.getItem('swu_auth');
-  //   if (token === null) {
-  //     return null;
-  //   }
-  //   return token;
-  // }
+  parseJwt(token) {
+    // const token = this.getJwt();
+    // if (!token) {
+    //   return null;
+    // }
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+  }
+  getJwt() {
+    const token = localStorage.getItem('swu_auth');
+    if (token === null) {
+      return null;
+    }
+    return token;
+  }
 }
