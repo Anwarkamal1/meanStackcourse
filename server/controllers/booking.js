50533c1b401a43d49f28ff6800c1c46babf54f5d
Anwarkamal1 <Anwar2lucky@gmail.com>
2019-04-01 18:54:44 +0500

    Optimizing dates

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -102,30 +102,68 @@ exports.createBooking = async (req, res, next) => {
 function isValidBooking(proposedBooking, rental) {
   let isValid = true;
   if (rental.bookings && rental.bookings.length > 0) {
-    const proposedStart = new Date(proposedBooking.startAt);
-    const proposedEnd = new Date(proposedBooking.endAt);
+    // const proposedStart = +new Date(proposedBooking.startAt);
+    // const proposedEnd = new Date(proposedBooking.endAt);
+    // console.log(proposedBooking.startAt);
+    // console.log(proposedBooking.endAt);
+    let startp = proposedBooking.startAt.split('T')[0].split('-');
+    let smonth = startp[1];
+    if (startp[1] > 0) {
+      smonth = +startp[1] - 1;
+    }
+    startp[1] = smonth;
+    let endp = proposedBooking.endAt.split('T')[0].split('-');
+    let emonth = endp[1];
+    if (endp[1] > 0) {
+      emonth = +endp[1] - 1;
+    }
+    endp[1] = emonth;
+    const proposedStart = +new Date(startp);
+    const proposedEnd = +new Date(endp);
+    console.log(proposedBooking.endAt);
     isValid = rental.bookings.every(booking => {
       let startd = booking.startAt.split('T')[0].split('-');
 
       let endd = booking.endAt.split('T')[0].split('-');
+      // console.log(startd);
+      // console.log(endd);
       let actualStart = -1;
       let actualEnd = -1;
       if (+startd[1] === 12) {
         start12 = false;
         actualStart = +new Date(`${+startd[0]}-12-${+startd[2]}`);
-      } else if (+endd[1] === 1) {
+        // console.log(new Date(`${+startd[0]}-12-${+startd[2]}`));
+        // console.log('in st 12');
+      } else if (+endd[1] === 01) {
+        // console.log('in st 1');
         actualStart = +new Date(`${+startd[0]}-01-${+startd[2]}`);
       } else {
-        actualStart = +new Date(booking.startAt);
+        // console.log('in st ');
+        actualStart = +new Date(
+          `${+startd[0]}-${startd[1] - 1}-${+startd[2] + 1}`
+        );
+        // console.log(
+        //   new Date(`${+startd[0]}-${startd[1] - 1}-${+startd[2] + 1}`)
+        // );
       }
       if (+endd[1] === 12) {
+        // console.log('in et 12');
+        // console.log(new Date(`${+endd[0]}-01-${+endd[2]}`));
         end12 = false;
         actualEnd = +new Date(`${+endd[0]}-12-${+endd[2]}`);
-      } else if (+endd[1] === 1) {
+      } else if (+endd[1] === 01) {
+        // console.log('in et 1');
+        // console.log(new Date(`${+endd[0]}-01-${+endd[2]}`));
         actualEnd = +new Date(`${+endd[0]}-01-${+endd[2]}`);
       } else {
-        actualEnd = +new Date(booking.endAt);
+        // console.log('in et ');
+        // console.log(new Date(`${+endd[0]}-${+endd[1] - 1}-${+endd[2] + 1}`));
+        actualEnd = +new Date(`${+endd[0]}-${+endd[1] - 1}-${+endd[2] + 1}`);
       }
+      // console.log(proposedEnd);
+      // console.log(proposedStart);
+      // console.log(actualStart);
+      // console.log(actualEnd);
       return (
         (actualStart < proposedStart && actualEnd < proposedEnd) ||
         (proposedEnd < actualEnd && proposedEnd < actualStart)
@@ -163,65 +201,85 @@ function populateDate(start, end, sfirstDay, sendDay, efirstDay, eendDay) {
   let start12 = true,
     end12 = true;
   let startAt, endAt;
-  console.log(start, end, sendDay, eendDay, efirstDay, efirstDay);
-  if (start[1] === '12') {
-    if (start[2] > -1 && start[2] < 10) {
-      startAt = `${+start[0]}-12-0${+start[2]}T19:00:00.000Z`;
-    } else {
-      startAt = `${+start[0]}-12-${+start[2]}T19:00:00.000Z`;
-    }
-    start12 = false;
+  // console.log(start, end, sendDay, eendDay, efirstDay, efirstDay);
+  if (+start[1] < 11 && +start[2] < 10) {
+    startAt = `${+start[0]}-0${+start[1]}-0${+start[2]}T19:00:00.000Z`;
+  } else if (+start[1] < 12) {
+    startAt = `${+start[0]}-0${+start[1]}-${+start[2]}T19:00:00.000Z`;
+  } else if (+start[2] < 10) {
+    startAt = `${+start[0]}-${+start[1]}-0${+start[2]}T19:00:00.000Z`;
+  } else {
+    startAt = `${+start[0]}-${+start[1]}-${+start[2]}T19:00:00.000Z`;
   }
-  if (end[1] === '12') {
-    end12 = false;
-    if (end[2] > -1 && end[2] < 10) {
-      endAt = `${+end[0]}-12-0${+start[2]}T19:00:00.000Z`;
-    } else {
-      endAt = `${+end[0]}-12-${+end[2]}T19:00:00.000Z`;
-    }
+  if (+end[1] < 11 && +end[2] < 10) {
+    endAt = `${+end[0]}-0${+end[1]}-0${+end[2]}T19:00:00.000Z`;
+  } else if (+end[1] < 12) {
+    endAt = `${+end[0]}-0${+end[1]}-${+end[2]}T19:00:00.000Z`;
+  } else if (+end[2] < 10) {
+    endAt = `${+end[0]}-${+end[1]}-0${+end[2]}T19:00:00.000Z`;
+  } else {
+    endAt = `${+end[0]}-${+end[1]}-${+end[2]}T19:00:00.000Z`;
   }
 
-  isstartTrue = false;
-  isend = false;
-  if (start12) {
-    if (+start[2] === sfirstDay) {
-      start[2] = 1 + '';
-      isstartTrue = true;
-      console.log('in firsts');
-    }
-    if (+start[2] === sendDay) {
-      start[2] = 1 + '';
-      start[1] = (+start[1] + 1).toString();
-      isstartTrue = true;
-    }
-  }
-  if (end12) {
-    if (+end[2] === efirstDay) {
-      end[2] = 2 + '';
-      isend = true;
-    }
-    if (+end[2] === eendDay) {
-      end[2] = 1 + '';
-      end[1] = (+end[1] + 1).toString();
-      isend = true;
-    }
-  }
-  if (isstartTrue) {
-    // start[2] = +start[2] + 1;
-    startAt = new Date(
-      start[0] + '-' + start[1] + '-' + start[2]
-    ).toISOString();
-  } else if (start12) {
-    startAt = new Date(
-      start[0] + '-' + start[1] + '-' + +start[2]
-    ).toISOString();
-  }
-  if (isend) {
-    end[2] = +end[2] + 1;
-    endAt = new Date(end[0] + '-' + end[1] + '-' + (+end[2] - 1)).toISOString();
-  } else if (end12) {
-    endAt = new Date(end[0] + '-' + end[1] + '-' + +end[2]).toISOString();
-  }
+  // if (start[1] === '12') {
+  //   if (start[2] > -1 && start[2] < 10) {
+  //     startAt = `${+start[0]}-12-0${+start[2]}T19:00:00.000Z`;
+  //   } else {
+  //     startAt = `${+start[0]}-12-${+start[2]}T19:00:00.000Z`;
+  //   }
+  //   start12 = false;
+  // }
+  // if (end[1] === '12') {
+  //   end12 = false;
+  //   if (end[2] > -1 && end[2] < 10) {
+  //     endAt = `${+end[0]}-12-0${+end[2]}T19:00:00.000Z`;
+  //   } else {
+  //     endAt = `${+end[0]}-12-${+end[2]}T19:00:00.000Z`;
+  //   }
+  // }
+
+  // isstartTrue = false;
+  // isend = false;
+  // if (start12) {
+  //   if (+start[2] === sfirstDay) {
+  //     start[2] = 1 + '';
+  //     isstartTrue = true;
+  //     console.log('in firsts');
+  //   }
+  //   if (+start[2] === sendDay) {
+  //     start[2] = sendDay + '';
+  //     start[1] = (+start[1] + 1).toString();
+  //     isstartTrue = true;
+  //   }
+  // }
+  // if (end12) {
+  //   if (+end[2] === efirstDay) {
+  //     end[2] = 2 + '';
+  //     isend = true;
+  //   }
+  //   if (+end[2] === eendDay) {
+  //     end[2] = 1 + '';
+  //     end[1] = (+end[1] + 1).toString();
+  //     isend = true;
+  //   }
+  // }
+  // if (isstartTrue) {
+  //   // start[2] = +start[2] + 1;
+  //   startAt = `${+start[0]}-12-0${+start[2]}T19:00:00.000Z`;
+  // } else if (start12) {
+  //   startAt = new Date(
+  //     start[0] + '-' + start[1] + '-' + +start[2]
+  //   ).toISOString();
+  // }
+  // if (isend) {
+  //   end[2] = +end[2] + 1;
+  //   endAt = new Date(end[0] + '-' + end[1] + '-' + (+end[2] - 1)).toISOString();
+  // } else if (end12) {
+  //   endAt = new Date(end[0] + '-' + end[1] + '-' + +end[2]).toISOString();
+  // }
   console.log(startAt, endAt);
-  return { startAt, endAt };
+  return {
+    startAt,
+    endAt
+  };
 }
--- src/app/common/service/helper.service.ts
+++ src/app/common/service/helper.service.ts
@@ -38,24 +38,50 @@ export class HelperService {
         customdates
     );
     while (mStartAt < mEndat) {
-      if (tempDates.indexOf(this.formatBookingDate(mStartAt)) === -1) {
-        let dates = this.formatBookingDate(mStartAt);
-        let check = dates.split(dateFormat);
-        if (+check[2] === 0) {
-          if (+check[1] === 0) {
-            tempDates.push(+check[0] - 1 + dateFormat + 11 + dateFormat + 31);
-            // console.log('hello', dates, new Date(+dates[0], 1, 0).getDate());
-          } else {
-            var slastDay = new Date(+dates[0], +dates[1], 0).getDate();
+      // let day = startdatesplits[2];
+      // let month = startdatesplits[1];
+      // let arrayd = Array.from(day);
+      // let arraym = Array.from(month);
+      // if (arrayd[0] === '0') {
+      //   day = startdatesplits[2][1];
+      // }
+      // if (arraym[0] === '0') {
+      //   month = startdatesplits[1][1];
+      // }
+      // tempDates.push(
+      //         check[0] + dateFormat + (+check[1] - 1) + dateFormat + slastDay
+      //       );
 
-            tempDates.push(
-              check[0] + dateFormat + (+check[1] - 1) + dateFormat + slastDay
-            );
-          }
-        } else {
-          tempDates.push(this.formatBookingDate(mStartAt));
-        }
+      if (tempDates.indexOf(this.formatBookingDate(mStartAt)) === -1) {
+        // let dates = this.formatBookingDate(mStartAt);
+        // let check = dates.split(dateFormat);
+        // if (+check[2] === 0) {
+        //   if (+check[1] === 0) {
+        //     tempDates.push(+check[0] - 1 + dateFormat + 11 + dateFormat + 31);
+        //     // console.log('hello', dates, new Date(+dates[0], 1, 0).getDate());
+        //   } else {
+        //     var slastDay = new Date(+dates[0], +dates[1], 0).getDate();
+        //     tempDates.push(
+        //       check[0] + dateFormat + (+check[1] - 1) + dateFormat + slastDay
+        //     );
+        //   }
+        // } else {
+        //   tempDates.push(this.formatBookingDate(mStartAt));
+        // }
       }
+      tempDates.push(
+        mStartAt.getFullYear() +
+          dateFormat +
+          mStartAt.getMonth() +
+          dateFormat +
+          mStartAt.getDate()
+      );
+      console.log(
+        mStartAt.getFullYear(),
+        mStartAt.getMonth(),
+        mStartAt.getDate()
+      );
+
       mStartAt.setDate(mStartAt.getDate() + 1);
     }
     tempDates.push(
